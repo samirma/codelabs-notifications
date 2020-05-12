7c5ed41ecf344901c5e4577385fe79c8df96f519
Samir Antonio <samir.ma@gmail.com>
2020-05-12 09:34:25 +0100

    feat: notification working

--- app/src/main/java/com/codelabs/notifyme/MainActivity.kt
+++ app/src/main/java/com/codelabs/notifyme/MainActivity.kt
@@ -1,12 +1,69 @@
 package com.codelabs.notifyme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat.Builder
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+
+    private var mNotifyManager: NotificationManager? = null
+
+
+    private val NOTIFICATION_ID = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notify.setOnClickListener {
+            sendNotification()
+        }
+
+
+        createNotificationChannel()
+
+        getNotificationBuilder()
+
     }
+
+    private fun getNotificationBuilder(): Builder {
+        return Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle("You've been notified!")
+            .setContentText("This is your notification text.")
+            .setSmallIcon(R.drawable.ic_android)
+    }
+
+    fun sendNotification() {
+
+        val notifyBuilder: Builder = getNotificationBuilder()
+        mNotifyManager!!.notify(NOTIFICATION_ID, notifyBuilder.build())
+
+    }
+
+    fun createNotificationChannel() {
+        mNotifyManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.O
+        ) {
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Mascot Notification", NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true);
+            notificationChannel.lightColor = Color.RED;
+            notificationChannel.enableVibration(true);
+            notificationChannel.description = "Notification from Mascot";
+            mNotifyManager?.createNotificationChannel(notificationChannel)
+        }
+    }
+
 }
